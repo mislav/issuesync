63bd846d05938847330406b93f9c38be5f02bd47
Mislav Marohnić <mislav.marohnic@gmail.com>
2012-11-03 13:52:29 +0100

    the issues, they are syncing

---
+++ issuesync.rb
@@ -0,0 +1,149 @@
+require 'net/http/persistent'
+require 'time'
+require 'json'
+require 'pathname'
+require 'fileutils'
+
+class IssueSync
+  def self.start(*args)
+    new(*args).start
+  end
+
+  attr_reader :repo, :path
+
+  def initialize repo, path
+    @repo = repo
+    @path = Pathname.new(path).expand_path
+  end
+
+  def start
+    # since: time.utc.iso8601
+    data = get "/repos/#{repo}/issues?state=open&sort=updated"
+    data += get "/repos/#{repo}/issues?state=closed&sort=updated"
+    issues = data.map {|entry| Issue.new entry }
+
+    FileUtils.mkdir_p path
+
+    for issue in issues
+      issue_file = path + "#{issue.number}.md"
+      next if issue_file.exist? and issue_file.mtime >= issue.updated_at
+      issue_file.open 'w' do |file|
+        title = "##{issue.number}: #{issue.title}"
+        file.puts title
+        file.puts "=" * title.size
+        file.puts
+        file.puts issue.body
+
+        if issue.has_comments?
+          data = get issue.comments_url
+          comments = data.map {|entry| Comment.new entry }
+
+          for comment in comments
+            file.puts
+            file.puts "## #{comment.user}"
+            file.puts
+            file.puts comment.body
+          end
+        end
+      end
+
+      if patch_url = issue.patch_url
+        patch = get_response patch_url
+        patch_file = Pathname.new issue_file.to_s.sub(/\.md$/, '.patch')
+        patch_file.open('w') {|f| f << patch.body }
+      end
+    end
+  end
+
+  def base_uri() URI 'https://api.github.com' end
+
+  def http
+    @http ||= begin
+      conn = Net::HTTP::Persistent.new self.class.name
+      conn.debug_output = $stderr if $DEBUG
+      conn
+    end
+  end
+
+  def headers
+    { 'Accept' => 'application/vnd.github.v3.raw+json',
+    }
+  end
+
+  def get path
+    data = nil
+    uri  = base_uri + path
+
+    while uri
+      res  = get_response uri
+      data = data ? data.concat(res.data) : res.data
+      uri  = res.next_url
+    end
+
+    data
+  end
+
+  def get_response uri
+    req = Net::HTTP::Get.new uri.request_uri, headers
+    $stderr.puts uri if $VERBOSE
+    res = ApiResponse.new http.request(uri, req)
+    res.response.error! unless res.success?
+    $stderr.puts "ratelimit remaining: %d" % res.ratelimit_remaining if $VERBOSE
+    res
+  end
+
+  ApiResponse = Struct.new(:response) do
+    def body() response.body end
+
+    def success?() Net::HTTPSuccess === response end
+
+    def data
+      @data ||= JSON.parse body
+    end
+
+    def links
+      response['link'].to_s.
+        scan(/<(.+?)>; rel="(.+?)"/).
+        each_with_object({}) {|(url, type), all| all[type] = url }
+    end
+
+    def ratelimit_remaining
+      response['x-ratelimit-remaining'].to_i
+    end
+
+    def next_url
+      if rel_next = links['next']
+        URI rel_next
+      end
+    end
+  end
+
+  Issue = Struct.new(:data) do
+    def number() data['number'] end
+    def title() data['title'] end
+    def body() data['body'].to_s.strip.gsub("\r\n", "\n") end
+    def user() data['user']['login'] end
+    def comments_url() URI(data['url'] + '/') + 'comments' end
+    def patch_url
+      url = data['pull_request']['patch_url'] and URI(url)
+    end
+    def updated_at() Time.parse(data['updated_at']) end
+    def has_comments?() !data['comments'].zero? end
+  end
+
+  class Comment < Issue
+    def body
+      super.
+        gsub(/^[> ]*On .+?<reply@reply\.github\.com>[>\s]+wrote:[>\s]*(\n|\Z)/m, '').
+        gsub(/^(>+\n)?>+ --\n>+ Reply to this email .+?https:\/\/github\.com\/.+?(\n|\Z)/m, '')
+    end
+  end
+end
+
+if __FILE__ == $0
+  File.readlines('.git/config').find {|rem| %r{github\.com[:/](.+?)/(.+?)(\.git)?$} =~ rem }
+  repo_with_owner = "#$1/#$2" if $1
+  abort "no GitHub repo found among git remotes" if repo_with_owner.nil?
+
+  IssueSync.start repo_with_owner, 'issues'
+end
